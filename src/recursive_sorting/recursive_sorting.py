a305f6b60f33ed0b01f71def79806d251daf98d9
ThisIsJorgeLima <ThisIsJorgeLima@users.noreply.github.com>
2020-05-19 20:48:43 -0400

    initial commit

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,16 +3,40 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
-    # Your code here
+    arrA_index = 0
+    arrB_index = 0
 
+    for i in range(elements):
+        if arrA_index >= len(arrA):
+            merged_arr[i] = arrB[arrB_index]
+            arrB_index += 1
+
+        elif counter_B >= len(arrB):
+            merged_arr[i] = arrA[arrA_index]
+            arrA_index += 1
+
+        elif arrA[arrA_index] < arrB[arrB_index]:
+            merged_arr[i] = arrA[arrA_index]
+            arrA_index += 1
+        else:
+            # if element in arrB is smaller, add it to merged_arr at that index
+            # location and move to next index.
+            merged_arr[i] = arrB[arrB_index]
+            arrB_index += 1
 
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    # Your code here
-
+    if len(arr) > 1:
+        # find the middle of the array [2, 7, 3]
+        # left = [2]
+        left = merge_sort(arr[0:len(arr)//2])
+        # right = [7, 3]
+        right = merge_sort(arr[len(arr)//2:])
+        # merge halves together
+        arr = merge(left, right)
 
     return arr
 
@@ -21,13 +45,11 @@ def merge_sort(arr):
 def merge_in_place(arr, start, mid, end):
     # Your code here
 
-
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
 
     return arr
 
